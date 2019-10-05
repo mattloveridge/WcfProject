c680d37bf3e1d6b875c09affda4ea34006d7641a
matt loveridge <mattloveridge@gmail.com>
2019-10-05 14:58:52 -0600

    pickup missed files

--- src/CellPhone.java
+++ src/CellPhone.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class CellPhone {
+
+    private int employeeId;
+    private String employeeName;
+    private int purchaseDate;
+    private String model;
+
+    public CellPhone(String [] strArray) {
+        employeeId = Integer.parseInt(strArray[0]);
+        employeeName = strArray[1];
+        purchaseDate = Integer.parseInt(strArray[2]);
+        model = strArray[3];
+    }
+
+    public int getEmployeeId() { return employeeId; }
+
+    public String getEmployeeName() { return employeeName; }
+
+    public int getPurchaseDate() { return purchaseDate; }
+
+    public String getModel() { return model; }
 }
--- src/CellPhoneDao.java
+++ src/CellPhoneDao.java
@@ -1,6 +1,33 @@
-public class DataStoresDao {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class CellPhoneDao {
 
+    public Map<Integer, CellPhone> loadCellPhoneDataStore(){
 
-    
+        String file = "C:\\WCF\\CellPhone.txt";
+
+        Map<Integer, CellPhone> cellPhoneMap = null;
+
+        try (Stream<String> stream = Files.lines(Paths.get(file))) {
+
+            cellPhoneMap = stream.skip(1).map(line -> {
+                return buildCellPhone(line);
+                } ) .
+                    collect(Collectors.toMap(cellPhone -> cellPhone.getEmployeeId(), cellPhone -> cellPhone));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return cellPhoneMap;
+    }
+
+    private CellPhone buildCellPhone(String line){
+        String [] strArray = line.split(",");
+        return new CellPhone(strArray);
+    }
 }
